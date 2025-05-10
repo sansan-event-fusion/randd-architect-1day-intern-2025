704d8993c87ee441acff55bd9169bf348cac8f36
山本大志 <boysbeambitious_0328@outlook.jp>
2025-05-10 16:53:08 +0900

    update app

--- app/main.py
+++ app/main.py
@@ -1,24 +1,90 @@
-import requests
 import streamlit as st
+from utils import (
+    get_cards_info,
+    get_contact_histories_by_user_id,
+    get_contact_histories_count,
+    get_max_contact_time,
+    get_owner_companies_by_company_id,
+    get_recommendation_users,
+    plot_contact_history_by_date,
+    plot_contact_history_by_time,
+)
 
-API_URL = "https://circuit-trial.stg.rd.ds.sansan.com/api/"
+menu = st.sidebar.radio("メニューを選択してください", ("名刺交換履歴", "おすすめの相手"))
 
-user_id = st.number_input("ユーザーIDを入力してください", min_value=0, max_value=10**10, value=0, step=1)
+if menu == "名刺交換履歴":
+    st.title("ユーザー名刺検索ツール")
+    st.caption("ユーザーIDの例: 7558936248")
 
-if st.button("検索"):
-    cards_url = API_URL + f"cards/{user_id}?offset=0&limit=10"
-    r = requests.get(cards_url, timeout=10)
-    owner_data = r.json()
-    for owner_data_item in owner_data:
-        st.write("会社名:" + owner_data_item["company_name"])
-        st.write("名前:" + owner_data_item["full_name"])
+    user_id = st.number_input("ユーザーIDを入力してください", min_value=0, max_value=10**10, value=0, step=1)
+
+    if st.button("検索"):
+        # 名刺情報取得
+        owner_data = get_cards_info(user_id)
+
+        if len(owner_data) == 0:
+            st.write("ユーザーが存在しませんでした。")
+        else:
+            st.subheader("ユーザー情報")
+            owner_name = owner_data[0]["full_name"]
+            company_name = owner_data[0]["company_name"]
+            company_id = owner_data[0]["company_id"]
+
+            st.write(f"👤 **名前**: {owner_name}")
+            st.write(f"🏢 **会社名**: {company_name}")
+
+            # 名刺枚数取得
+            data_owner_count = get_contact_histories_count(user_id)
+            st.write(f"💼 **交換した名刺の枚数**: {data_owner_count}")
+
+            # 名刺情報取得
+            st.subheader("ユーザーの名刺交換情報")
+            data_owner = get_contact_histories_by_user_id(user_id)
+            st.write("日付ごとの名刺交換回数")
+            plot_contact_history_by_date(data_owner)
+            st.write("時間帯ごとの名刺交換回数")
+            plot_contact_history_by_time(data_owner)
+
+            # 会社の名刺情報取得
+            st.subheader("ユーザーの会社の名刺交換情報")
+            data_company = get_owner_companies_by_company_id(company_id)
+            st.write("日付ごとの名刺交換回数")
+            plot_contact_history_by_date(data_company)
+            st.write("時間帯ごとの名刺交換回数")
+            plot_contact_history_by_time(data_company)
+
+            with st.expander("名刺交換相手を表示"):
+                if len(data_owner) == 0:
+                    st.write("名刺情報は見つかりませんでした。")
+                else:
+                    for data_owner_item in data_owner:
+                        card_user_id = data_owner_item["user_id"]
+                        owner_data = get_cards_info(card_user_id)
+                        st.write(f"👤 **名前**: {owner_data[0]['full_name']}")
+                        st.write(f"🏢 **会社名**: {owner_data[0]['company_name']}")
+                        st.write("")
+else:
+    st.title("おすすめの相手")
+    st.caption("ユーザーIDの例: 7558936248")
+
+    user_id = st.number_input("ユーザーIDを入力してください", min_value=0, max_value=10**10, value=0, step=1)
+    # おすすめユーザー
+    data_recommendation = get_recommendation_users(user_id)
+
+    if len(data_recommendation) == 0:
+        st.info("おすすめの相手は見つかりませんでした。")
+    else:
         st.write("")
+        for data_recommendation_item in data_recommendation:
+            recommendation_user_id = data_recommendation_item["user_id"]
+            data_owner = get_contact_histories_by_user_id(recommendation_user_id)
+            st.write(f"🏢 **会社名**: {data_recommendation_item['company_name']}")
+            st.write(f"👤 **名前**: {data_recommendation_item['full_name']}")
 
-    st.write("おすすめの相手")
-    cards_recommendation_url = API_URL + f"cards/{user_id}/similar_top10_users"
-    r = requests.get(cards_recommendation_url, timeout=10)
-    data_recommendation = r.json()
-    for data_recommendation_item in data_recommendation:
-        st.write("会社名:" + data_recommendation_item["company_name"])
-        st.write("名前:" + data_recommendation_item["full_name"])
-        st.write("")
+            recommendation_time = get_max_contact_time(data_owner)
+            st.write(f"📅 **交換におすすめの時間**: {recommendation_time['hour']}:00")
+
+            with st.expander(f"{data_recommendation_item['full_name']}さんの名刺交換情報"):
+                st.write("時間帯ごとの名刺交換回数")
+                plot_contact_history_by_time(data_owner)
+            st.write("")
---
+++ app/utils.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import requests
+import streamlit as st
+
+API_URL = "https://circuit-trial.stg.rd.ds.sansan.com/api/"
+
+
+def plot_contact_history_by_date(data):
+    created_at_list = [item["created_at"] for item in data if "created_at" in item]
+    df_created_at_list = pd.DataFrame({"created_at": pd.to_datetime(created_at_list)})
+
+    # 週単位の集計:週の開始日(月曜)を基準に集計
+    df_created_at_list["week_start"] = df_created_at_list["created_at"].dt.to_period("W").apply(lambda r: r.start_time)
+
+    df_count = df_created_at_list.groupby("week_start").size().reset_index(name="交換回数")
+    df_count["week_start"] = pd.to_datetime(df_count["week_start"])
+
+    # グラフ表示
+    st.bar_chart(data=df_count.set_index("week_start"))
+
+
+def plot_contact_history_by_time(data):
+    created_at_list = [item["created_at"] for item in data if "created_at" in item]
+    df_created_at_list = pd.DataFrame({"created_at": pd.to_datetime(created_at_list)})
+    df_created_at_list["created_at"] = df_created_at_list["created_at"].dt.tz_convert("Asia/Tokyo")
+
+    # 時刻(hour単位)を抽出
+    df_created_at_list["hour"] = df_created_at_list["created_at"].dt.hour
+
+    # 時間ごとの交換回数を集計
+    df_count = df_created_at_list.groupby("hour").size().reset_index(name="交換回数")
+
+    df_count["時刻"] = df_count["hour"].apply(lambda x: f"{x:02d}:00")
+    df_count = df_count.sort_values("hour")
+
+    # グラフ表示
+    st.bar_chart(data=df_count.set_index("時刻")["交換回数"])
+
+
+# 一番交換回数が多い時間帯を取得
+def get_max_contact_time(data):
+    created_at_list = [item["created_at"] for item in data if "created_at" in item]
+    df_created_at_list = pd.DataFrame({"created_at": pd.to_datetime(created_at_list)})
+    df_created_at_list["created_at"] = df_created_at_list["created_at"].dt.tz_convert("Asia/Tokyo")
+
+    # 時刻(hour単位)を抽出
+    df_created_at_list["hour"] = df_created_at_list["created_at"].dt.hour
+
+    # 時間ごとの交換回数を集計
+    df_count = df_created_at_list.groupby("hour").size().reset_index(name="交換回数")
+
+    max_row = df_count.loc[df_count["交換回数"].idxmax()]
+    return max_row
+
+
+def get_contact_histories_by_user_id(user_id):
+    cards_owner_url = API_URL + f"contacts/owner_users/{user_id}"
+    r = requests.get(cards_owner_url, timeout=10)
+    data = r.json()
+    return data
+
+
+def get_cards_info(user_id):
+    cards_url = API_URL + f"cards/{user_id}"
+    r = requests.get(cards_url, timeout=10)
+    data = r.json()
+    return data
+
+
+def get_contact_histories_count(user_id):
+    cards_owner_count_url = API_URL + f"contacts/owner_users/{user_id}/count"
+    r = requests.get(cards_owner_count_url, timeout=10)
+    data_owner_count = r.json()
+    return data_owner_count
+
+
+def get_recommendation_users(user_id):
+    cards_recommendation_url = API_URL + f"cards/{user_id}/similar_top10_users"
+    r = requests.get(cards_recommendation_url, timeout=10)
+    data_recommendation = r.json()
+    return data_recommendation
+
+
+def get_owner_companies_by_company_id(company_id):
+    cards_company_url = API_URL + f"contacts/owner_companies/{company_id}"
+    r = requests.get(cards_company_url, timeout=10)
+    data_company = r.json()
+    return data_company
