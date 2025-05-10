29f67477523ca747424b1215454142787f328eb7
山本大志 <boysbeambitious_0328@outlook.jp>
2025-05-10 14:31:45 +0900

    create my app

--- app/main.py
+++ app/main.py
@@ -1,12 +1,27 @@
 from pathlib import Path
-
+import requests
 import pandas as pd
 import streamlit as st
 
 # タイトル
-st.title("サンプルアプリ")
+API_URL = "https://circuit-trial.stg.rd.ds.sansan.com/api/"
 
-path = Path(__file__).parent / "dummy_data.csv"
-df_dummy = pd.read_csv(path, dtype=str)
+user_id = st.number_input("ユーザーIDを入力してください", min_value=0, max_value=10**10, value=0, step=1)
 
-st.dataframe(df_dummy)
+if st.button("検索"):
+    cards_url = API_URL + f"cards/{user_id}?offset=0&limit=10"
+    r = requests.get(cards_url, timeout=10)
+    owner_data = r.json()
+    for owner_data_item in owner_data:
+        st.write("会社名:" + owner_data_item["company_name"])
+        st.write("名前:" + owner_data_item["full_name"])
+        st.write("")
+
+    st.write("おすすめの相手")
+    cards_recommendation_url = API_URL + f"cards/{user_id}/similar_top10_users"
+    r = requests.get(cards_recommendation_url, timeout=10)
+    data_recommendation = r.json()
+    for data_recommendation_item in data_recommendation:
+        st.write("会社名:" + data_recommendation_item["company_name"])
+        st.write("名前:" + data_recommendation_item["full_name"])
+        st.write("")
