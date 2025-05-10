6149da760cdd7b02973220494911c0f6f88faf0b
あなたのGitHubユーザー名 <>
2025-05-10 14:59:38 +0900

    main modifyed

--- app/main.py
+++ app/main.py
@@ -1,12 +1,4 @@
-from pathlib import Path
+from data_visualization import main
 
-import pandas as pd
-import streamlit as st
-
-# タイトル
-st.title("サンプルアプリ")
-
-path = Path(__file__).parent / "dummy_data.csv"
-df_dummy = pd.read_csv(path, dtype=str)
-
-st.dataframe(df_dummy)
+if __name__ == "__main__":
+    main()
