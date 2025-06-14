c2c81d59e1b5032d4ef31c3f7a5d9c147b8f3962
HarutoKobayashi <kobayashi.haruto.t8@dc.tohoku.ac.jp>
2025-06-14 15:18:23 +0900

    CI を試すテスト用アプリ

--- app/main.py
+++ app/main.py
@@ -1,12 +1,22 @@
-from pathlib import Path
-
 import pandas as pd
+import requests
 import streamlit as st
 
 # タイトル
-st.title("サンプルアプリ")
+st.title("テスト")
 
-path = Path(__file__).parent / "dummy_data.csv"
-df_dummy = pd.read_csv(path, dtype=str)
+# テキストボックス（文字列入力）
+user_id = st.text_input("user_idを入力してください")
 
-st.dataframe(df_dummy)
+# ボタンを押したときに、入力値を使って処理を実行
+if st.button("実行"):
+    if user_id:
+        st.write(f"こんにちは、{user_id}さん！")
+        # ここに任意の処理を記述（例: 検索、計算など）
+        url = f"https://circuit-trial.stg.rd.ds.sansan.com/api/cards/{user_id}/similar_top10_users"
+        response = requests.get(url)
+        results: list[dict] = response.json()
+        df = pd.DataFrame(results)
+        st.dataframe(df)
+    else:
+        st.warning("user_idを入力してください。")
