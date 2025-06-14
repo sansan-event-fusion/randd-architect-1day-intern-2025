7f2f99473f2ddcd4452b6bad0717a283f98dc221
Yamaguchi447 <yamayou2018@gmail.com>
2025-06-14 16:03:56 +0900

    Update app/main.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- app/main.py
+++ app/main.py
@@ -159,7 +159,7 @@ trade_cnt['industry'] = trade_cnt['cluster'].map(lambda i: cluster_names[i])
 # 可視化
 st.write(f"### {sel_company} の取引業種分布")
 fig, ax = plt.subplots(figsize=(8,5))
-ax.bar(trade_cnt['industry'], trade_cnt['count'])
+ax.bar(trade_cnt["industry"], trade_cnt["count"])
 ax.set_ylabel("取引件数（カード交換数）")
 ax.set_xticklabels(trade_cnt["industry"], rotation=45, ha="right")
 st.pyplot(fig)
