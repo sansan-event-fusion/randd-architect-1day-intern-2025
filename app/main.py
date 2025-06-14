4e927c0c8987bb2b8c1ebb6bd19567ecdbf75ae4
west0714 <t21060tt@gmail.com>
2025-06-14 17:17:57 +0900

    create my app

--- app/main.py
+++ app/main.py
@@ -13,20 +13,26 @@ input_date = st.date_input("いつ以降のデータを取得しますか？", v
 # ボタンを押したらAPIを呼び出す
 if st.button("検索"):
     # 入力されたuser_idを使用してAPIを呼び出すlimitを10に設定
-    owner_url=f"https://circuit-trial.stg.rd.ds.sansan.com/api/contacts/owner_users/{input_user_id}?limit=10&start_date={input_date}"
+    owner_url=f"https://circuit-trial.stg.rd.ds.sansan.com/api/contacts/owner_users/{input_user_id}?limit=100&start_date={input_date}"
+    owner_all_url = f"https://circuit-trial.stg.rd.ds.sansan.com/api/contacts/owner_users/{input_user_id}?limit=100"
     try:
-        response = requests.get(owner_url, timeout=10)
+        response = requests.get(owner_url, timeout=30)
         response.raise_for_status()  # エラーハンドリング
 
         data = response.json()
         df = pd.DataFrame(data)
         user_ids = df["user_id"].unique()  # ユニークな user_id を取得
-
+        #やりとりをしたユーザーの情報を取得
+        all_response = requests.get(owner_all_url, timeout=30)
+        all_response.raise_for_status()  # エラーハンドリング
+        all_data = all_response.json()
+        all_df = pd.DataFrame(all_data)
+        all_user_ids = all_df["user_id"].unique()  # 全てのユニークな user_id を取得
         user_info_list = []
         for user_id in user_ids:
             user_info_url = f"https://circuit-trial.stg.rd.ds.sansan.com/api/cards/{user_id}"
             try:
-                user_info_response = requests.get(user_info_url, timeout=10)
+                user_info_response = requests.get(user_info_url, timeout=30)
                 user_info_response.raise_for_status()  # エラーハンドリング
 
                 user_info_data = user_info_response.json()
@@ -43,14 +49,15 @@ if st.button("検索"):
         for user_id in user_ids:
             similer_user_url = f"https://circuit-trial.stg.rd.ds.sansan.com/api/cards/{user_id}/similar_top10_users"
             try:
-                similer_response = requests.get(similer_user_url, timeout=10)
+                similer_response = requests.get(similer_user_url, timeout=30)
                 similer_response.raise_for_status()# エラーハンドリング
                 similer_data=similer_response.json()
                 similer_df=pd.DataFrame(similer_data)
                 target_user = user_info_df[user_info_df["user_id"] == user_id]
                 user_name = target_user["full_name"].values[0] if not target_user.empty else "不明"
                 st.subheader(f"ユーザーID: {user_name} ({user_id}) の類似ユーザー情報")
-                # 類似ユーザーの情報を表示
+                # all_user_idにない類似ユーザーの情報を表示
+                similer_df = similer_df[~similer_df["user_id"].isin(all_user_ids)]
                 st.dataframe(similer_df)
             except requests.exceptions.RequestException as e:
                 st.error(f"類似ユーザーの情報取得に失敗しました:{e}")
